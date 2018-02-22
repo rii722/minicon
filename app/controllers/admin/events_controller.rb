c47fd217f623232c31c8b3b2e473d274b80308dd
rii722 <rii722@users.noreply.github.com>
2018-02-22 08:11:18 +0000

    ホーム画面にイベントを表示させた

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,11 +1,16 @@
 class Admin::EventsController < ApplicationController
+  layout 'admin'
+  
   def index
+    @events = Event.all
   end
 
   def show
+    @event = Event.find(params[:id])
   end
 
   def new
+    @event = Event.new
   end
 
   def edit
