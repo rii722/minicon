89502085f305c8d143f91aeb4807a5127890e52b
rii722 <rii722@users.noreply.github.com>
2018-02-22 09:37:53 +0000

    編集、削除などの機能を追加した。

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,12 +1,14 @@
 class Admin::EventsController < ApplicationController
   layout 'admin'
   
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
+  
   def index
     @events = Event.all
   end
 
   def show
-    @event = Event.find(params[:id])
+    # @event = Event.find(params[:id])
   end
 
   def new
@@ -14,5 +16,41 @@ class Admin::EventsController < ApplicationController
   end
 
   def edit
+    @event = Event.find(params[:id])
+  end
+  
+  def create
+    # event_params = params.require(:event).permit(:title, :location, :start_at, :ticket_price, :ticket_quantity, :event_category_id)
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to admin_event_path(@event), notice: 'Event was successfully created.'
+    else
+      flash.now[:alert] = 'Event could not be created.'
+      render :new
+    end
+  end
+  
+  def update
+    # event_params = params.require(:event).permit(:title, :location, :start_at, :ticket_price, :ticket_quantity, :event_category_id)
+    if @event.update(event_params)
+      redirect_to admin_event_path(@event), notice: 'Event was successfully updaed.'
+    else
+      flash.now[:alert] = 'Event coould not be updated.'
+      render :edit
+    end
+  end
+  
+  def destroy
+    @event.destroy
+    redirect_to admin_events_url, notice: 'Event was successfully destroyed.'
+  end
+  
+  private
+  def set_event
+    @event = Event.find(params[:id])
+  end
+  
+  def event_params
+    params.require(:event).permit(:title, :location, :start_at, :ticket_price, :ticket_quantity, :event_category_id)
   end
 end
